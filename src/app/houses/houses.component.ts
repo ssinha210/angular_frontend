cd5317727f604f5556d31baa6727c3ae6b159599
ssinha210 <sankarshansinha210@gmail.com>
2024-07-09 17:18:59 +0530

    message

---
+++ src/app/houses/houses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-houses',
+  standalone: true,
+  imports: [],
+  templateUrl: './houses.component.html',
+  styleUrl: './houses.component.css'
+})
+export class HousesComponent {
+
+}
