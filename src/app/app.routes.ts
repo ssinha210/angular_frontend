457248f8f3254bb114de06a9e272a391a1684f09
ssinha210 <sankarshansinha210@gmail.com>
2024-07-10 12:43:17 +0530

    message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HousesComponent } from './houses/houses.component';
+import { ApplyComponent } from './apply/apply.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'house', component: HousesComponent },
+    { path: 'apply', component: ApplyComponent },
+
 
 
 ];
